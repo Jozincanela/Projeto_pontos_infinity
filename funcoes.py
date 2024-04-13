1b8a94c7daef691bd1bc80ebd6262e58f2f05e55
Jozincanela <Afrodrigues2004@gmail.com>
2024-04-13 14:43:21 -0300

    Versão 0.0.2

---
+++ funcoes.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import streamlit as st
+
+
+def saber_numero_mes(mes:str) -> int:
+        match mes.lower():
+            case "janeiro":
+                return 1
+            case "fevereiro":
+                return 2
+            case "marco":
+                return 3
+            case "abril":
+                return 4
+            case "maio":
+                return 5
+            case "junho":
+                return 6
+            case "julho":
+                return 7
+            case "agosto":
+                return 8
+            case "setembro":
+                return 9
+            case "outubro":
+                return 10
+            case "novembro":
+                return 11
+            case "dezembro":
+                return 12
+              
+def ler_arquivo_excel(caminho_arquivo: str, Nome: str = None, Mes: str = None):
+    
+    
+    """
+    Lê um arquivo Excel e retorna todas as linhas ou linhas filtradas com base na especificação.
+
+    Args:
+        caminho_arquivo (str): Caminho para o arquivo Excel.
+        Nome (str, optional): Valor específico para filtrar as linhas por nome. Padrão é None.
+        Mes (str, optional): Valor específico para filtrar as linhas por mês. Padrão é None.
+        Dia (int, optional): Valor específico para filtrar as linhas por dia. Padrão é None.
+
+    Returns:
+        pandas.DataFrame: DataFrame com as linhas lidas ou linhas filtradas.
+    """
+    df = pd.read_excel(caminho_arquivo)
+
+    # Convertendo a coluna 'DateTime' para datetime
+    df['DateTime'] = pd.to_datetime(df['DateTime'], format='%Y/%m/%d %H:%M')
+
+    if Nome is not None:
+        linhas_filtradas = df[df['Name'] == Nome]
+        return linhas_filtradas
+    elif Mes is not None and Nome is None:
+        num_mes = saber_numero_mes(Mes) 
+        linhas_filtradas = df[df['DateTime'].dt.month == num_mes]
+        return linhas_filtradas
+    elif Mes is not None and Nome is not None :
+        num_mes = saber_numero_mes(Mes) 
+        linhas_filtradas = df[(df['Name'] == Nome and df['DateTime']== num_mes )]
+        return linhas_filtradas
+    else:
+        return df
+    
+def ler_coluna_excel(caminho_arquivo: str, Coluna: str):
+    """
+    Lê todas as linhas de uma coluna específica de um arquivo Excel e retorna uma lista com valores únicos.
+
+    Args:
+        caminho_arquivo (str): Caminho para o arquivo Excel.
+        Coluna (str): Nome da coluna a ser lida.
+
+    Returns:
+        list: Lista com valores únicos da coluna.
+    """
+    try:
+        df = pd.read_excel(caminho_arquivo)
+        valores_coluna = df[Coluna].tolist()
+        valores_unicos = list(set(valores_coluna))
+        return valores_unicos
+    except FileNotFoundError:
+        print(f"Arquivo '{caminho_arquivo}' não encontrado.")
+        return []
+
--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import pandas as pd
 import streamlit as st
-CAMINHO_ARQUIVO = "pontoagoravai.xlsx"
-@st.cache_data#decorator --> linha de codigo que adiciona uma funcionalidade para funcao que vem logo em baixo , vantagens ---> quando aplicamos ele, ele armazena no cache do usuario as informações que ele esta carreagando aqui , primeira vez geralmente demora mas depois ele flui melhor 
-def ler_arquivo_exel(caminho_arquivo:str):
-    ler = pd.read_excel(caminho_arquivo)
-    pd.set_option('display.max_rows',None)# Mostra todas as linhas, sem abreviar nenhuma
-    return ler
+import funcoes
+@st.cache_data
+#decorator --> linha de codigo que adiciona uma funcionalidade para funcao que vem logo em baixo , vantagens ---> quando aplicamos ele, ele armazena no cache do usuario as informações que ele esta carreagando aqui , primeira vez geralmente demora mas depois ele flui melhor 
 
+def o():...
+
+CAMINHO_ARQUIVO = "pontoagoravai.xlsx"
+lista_meses =  [None,"Janeiro","Fevereiro","Marco","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro" ]
+
+lista_todos_nomes = funcoes.ler_coluna_excel(CAMINHO_ARQUIVO, 'Name')
+lista_todos_nomes.sort()
+lista_todos_nomes.insert(0, None)
 
 st.set_page_config(page_title="Projeto Controle de Ponto Infinity Bh")
 with st.container():
     st.title("Controle de ponto Infinity BH")
     st.subheader("Registros de Controle de Ponto")
 
-#menu para seleção de uma pessoa especifico
-selecao_pessoa = st.sidebar.selectbox('Selecione Um Funcionario',['ALEX','ANA PIO','ANDRE F','ARTHUR','Barbara','Bruna A','CARLOS NOVAI','CECILIA LIZ','DERECK','Daniele','GABI','GABRIEL','GIANELL','HENRIQUE MUN','ISRAEL','Italo','JEAN B','LUCAS','MAIRA v','MARIA E','MARIANA','PEDRO LOYOLA','PEDROB','RAPHAEL','ROBERTO','ROCK','WASHINGTON LU','arthurd','joao V','livia'])
 
+selecao_pessoa = st.sidebar.selectbox('Selecione Um Funcionario', lista_todos_nomes)
+selecao_mes =  st.sidebar.selectbox('Selecione um mes', lista_meses)
+tabela = funcoes.ler_arquivo_excel(CAMINHO_ARQUIVO, selecao_pessoa, selecao_mes)
 
-#Tela principal
 with st.container():
     st.write("---")
-    tabela = ler_arquivo_exel(CAMINHO_ARQUIVO)
-    
     ponto_por_pessoa = tabela[['Name','DateTime']]
-    #relacionando as datas com pessoas
     try:
         st.bar_chart(ponto_por_pessoa,x="DateTime",y="Name")
     except:
